3e347f0fa9ed3da48eeecef63aea542e7291990f
Joe Hills <joe@teamsnowday.com>
2015-01-21 20:33:40 -0500

    Added tsdrpg_species_summary post type

--- team_snow_day_rpg.php
+++ team_snow_day_rpg.php
@@ -109,7 +109,7 @@ function tsdrpg_class_summary() {
 		'show_in_nav_menus'   => true,
 		'show_in_admin_bar'   => true,
 		'menu_position'       => 20,
-		'menu_icon'           => 'dashicons dashicons-businessman',
+		'menu_icon'           => 'dashicons-businessman',
 		'can_export'          => true,
 		'has_archive'         => true,
 		'exclude_from_search' => false,
@@ -123,4 +123,47 @@ function tsdrpg_class_summary() {
 // Hook into the 'init' action
 add_action( 'init', 'tsdrpg_class_summary', 0 );
 
+// Register Custom Post Type
+function tsdrpg_species_summary() {
 
+	$labels = array(
+		'name'                => _x( 'Species Summaries', 'Post Type General Name', 'tsdrpg' ),
+		'singular_name'       => _x( 'Species Summary', 'Post Type Singular Name', 'tsdrpg' ),
+		'menu_name'           => __( 'Species Summaries', 'tsdrpg' ),
+		'parent_item_colon'   => __( 'Parent Species:', 'tsdrpg' ),
+		'all_items'           => __( 'All Species Summaries', 'tsdrpg' ),
+		'view_item'           => __( 'View Species Summary', 'tsdrpg' ),
+		'add_new_item'        => __( 'Add New Species Summary', 'tsdrpg' ),
+		'add_new'             => __( 'Add New', 'tsdrpg' ),
+		'edit_item'           => __( 'Edit Species Summary', 'tsdrpg' ),
+		'update_item'         => __( 'Update Species Summary', 'tsdrpg' ),
+		'search_items'        => __( 'Search Species Summaries', 'tsdrpg' ),
+		'not_found'           => __( 'Not found', 'tsdrpg' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'tsdrpg' ),
+	);
+	$args = array(
+		'label'               => __( 'tsdrpg_species_summary', 'tsdrpg' ),
+		'description'         => __( 'Basic summary page for a given species', 'tsdrpg' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'page-attributes', ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 20,
+		'menu_icon'           => 'dashicons-groups',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'tsdrpg_species_summary', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'tsdrpg_species_summary', 0 );
